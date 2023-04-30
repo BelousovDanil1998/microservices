7bcfb9e56068770375f9b014eaff14d427182bc6
Danil Belousov <eternitytinrete@mail.ru>
2023-04-30 03:11:46 +0300

    add MongoDB service

---
+++ BookService/src/main/java/service/BookServiceImpl.java
@@ -0,0 +1,21 @@
+package service;
+
+import model.Book;
+import org.springframework.stereotype.Service;
+import repository.BookRepository;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+}
